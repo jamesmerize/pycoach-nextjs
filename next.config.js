1550e9c3f73dc99f5d565f6f5eab7185eba29c3c
James Mascarenhas <engg.james@gmail.com>
2025-08-08 18:39:07 +0530

    url issue fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  basePath: '/pycoach-nextjs',
+  assetPrefix: '/pycoach-nextjs/',
   reactStrictMode: true,
   swcMinify: true,
   images: {
